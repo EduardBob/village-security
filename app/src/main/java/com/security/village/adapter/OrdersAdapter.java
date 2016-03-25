6440782c252644b4a6908593e085f555c05e3abf
Jenea Vranceanu <vranceanu.j@fruitware.ru>
2016-03-25 16:41:40 +0200

    icons problem fixed

--- app/src/main/java/com/security/village/adapter/OrdersAdapter.java
+++ app/src/main/java/com/security/village/adapter/OrdersAdapter.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.security.village.ObjectMap;
 import com.security.village.R;
