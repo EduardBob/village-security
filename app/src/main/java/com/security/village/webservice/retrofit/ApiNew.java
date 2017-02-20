f0fd0d7f5046fe158cd564fdd2b5cfbe5e739075
Jenea Vranceanu <vranceanu.j@fruitware.ru>
2017-02-20 19:42:37 +0200

    commit

--- app/src/main/java/com/security/village/webservice/retrofit/ApiNew.java
+++ app/src/main/java/com/security/village/webservice/retrofit/ApiNew.java
@@ -5,9 +5,9 @@ package com.security.village.webservice.retrofit;
  */
 public class ApiNew {
     //staging
-//    public static final String SERVER_URL = "http://village.fruitware.ru";
+    public static final String SERVER_URL = "http://village.fruitware.ru";
 //    production
-    public static final String SERVER_URL = "http://1centr.com";
+//    public static final String SERVER_URL = "http://1centr.com";
 
     public static final String LOGIN_URL = "api/v1/security/auth/token";
     public static final String REFRESH_TOKEN = "pi/v1/auth/refresh";
